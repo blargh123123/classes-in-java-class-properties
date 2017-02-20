0dcdfc7deb2b19a82aa0405e6878307507972e82
Doug Hughes <dhughes@doughughes.net>
2017-02-20 08:37:59 -0500

    complete exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,19 +9,33 @@ public class Main {
     public static void main(String[] args) {
 
         // todo: create an new variable "planet1" and set it to a new instance of the Planet class
+        Planet planet1 = new Planet();
 
         // todo: set all four properties of the Planet class on the planet1 instance: name, distanceFromSun, satellites, and supportsLife.
+        planet1.name="Earth";
+        planet1.distanceFromSun = 1;
+        planet1.satellites= 1;
+        planet1.supportsLife = true;
 
         // todo: create another new variable named "planet2" and set it to a new instance of the Planet class
+        Planet planet2 = new Planet();
 
         // todo: set all four properties of the Planet class on the planet2 instance
+        planet2.name="Fred";
+        planet2.distanceFromSun = 1.1;
+        planet2.satellites= 12;
+        planet2.supportsLife = false;
+
 
         // todo: Use println() to print a String description of planet1 using string concatenation
         /*
             Example output: "Name: Earth, Distance from Sun: 1 AU, Satellites: 1 moon(s), Supports Life: true"
          */
 
+        System.out.println("Name: " + planet1.name + ", Distance from Sun: " + planet1.distanceFromSun + " AU, Satellites: " + planet1.satellites + " moon(s), Supports Life: " + planet1.supportsLife );
+
         // todo: Use println() to print a String description of planet2, just like above
+        System.out.println("Name: " + planet2.name + ", Distance from Sun: " + planet2.distanceFromSun + " AU, Satellites: " + planet2.satellites + " moon(s), Supports Life: " + planet2.supportsLife );
 
         /*
             Next, we're going to demonstrate that properties of an object can be
@@ -30,8 +44,10 @@ public class Main {
          */
 
         // todo: change a single property on your planet1 instance
+        planet1.name = "New Earth";
 
         // todo: change a different property on your planet2 instance
+        planet2.satellites = 234;
 
         // todo: Use println() to print a String description of planet1 again.
         /*
@@ -40,8 +56,12 @@ public class Main {
             shows that these two instances are distinct and separate from each
             other.
          */
+        System.out.println("Name: " + planet1.name + ", Distance from Sun: " + planet1.distanceFromSun + " AU, Satellites: " + planet1.satellites + " moon(s), Supports Life: " + planet1.supportsLife );
+
 
         // todo: Use println() to print a String description of planet2, again.  Note that the property you changed on planet2 is reflected in the output.
+        System.out.println("Name: " + planet2.name + ", Distance from Sun: " + planet2.distanceFromSun + " AU, Satellites: " + planet2.satellites + " moon(s), Supports Life: " + planet2.supportsLife );
+
 
     }
 
